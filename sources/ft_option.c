8a3119424dbb0bc3d64c997a78b9b95366d11496
apierre42 <aurel.chenuil@gmail.com>
2015-03-25 19:17:42 +0100

    ft_option.c

---
+++ sources/ft_option.c
@@ -0,0 +1,42 @@
+int				get_opt(char *av[], t_opt *opt, int *i)
+{
+	char		*ret;
+
+	if (!(*i))
+	{
+		if (av[opt->nb][0] == '-' && av[opt->nb][1] != '-' && av[opt->nb][1])
+			(*i)++;
+		else
+		{
+			if (av[opt->nb][0] == '-' && av[opt->nb][1] == '-')
+			{
+				if (av[opt->nb][2])
+				{
+					opt->err = '-';
+					return ('?');
+				}
+				opt->nb++;
+			}
+			return (-1);
+		}
+	}
+	if (!(ret = ft_strchr(opt->optstr, av[opt->nb][(*i)++])))
+		opt->err = av[opt->nb][(*i) - 1];
+	return (!ret ? '?' : *ret);
+}
+
+int				ft_get_opt(int argc, char *av[], t_opt *opt)
+{
+	static int	i = 0;
+
+	if (!av[opt->nb][i])
+	{
+		if (!i)
+			return (0);
+		i = 0;
+		opt->nb++;
+		if (opt->nb == argc)
+			return (-1);
+	}
+	return (get_opt(av, opt, &i));
+}
