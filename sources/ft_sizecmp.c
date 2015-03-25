e3b929939343addee194908b1a5187f2f1f7893e
apierre42 <aurel.chenuil@gmail.com>
2015-03-25 14:50:04 +0100

    ft_sizecmp.c

--- sources/ft_sizecmp.c
+++ sources/ft_sizecmp.c
@@ -5,6 +5,17 @@ int		ft_sizecmp(void	*file1, void *file2)
 
 	f1 = (t_file *)file1;
 	f2 = (t_file *)file2;
-	return(f1->stats.st_size - f2->stats.st_size ?
-	f1->stats.st_size - f2->stats.st_size : ft_sizecmp(file1, file2));
+	return (f2->stats.st_size - f1->stats.st_size ?
+	f2->stats.st_size - f1->stats.st_size : ft_sizecmp(file1, file2));
+}
+
+int		ft_rsizecmp(void *file1, void *file2)
+{
+	t_file *f1;
+	t_file *f2;
+
+	f1 = (t_file *)file1;
+	f2 = (t_file *)file2;
+	return (f1->stats.st_size -f2->stats.st_size ?
+	f1->stats.st_size - f2->stats.st_size : ft_strcmp(file1, file2));
 }
