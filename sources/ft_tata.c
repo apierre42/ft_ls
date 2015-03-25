2ac8e0c9003b9d75487d888052e9a39e068013bf
apierre42 <aurel.chenuil@gmail.com>
2015-03-25 19:32:12 +0100

    ft_tata.c

---
+++ sources/ft_tata.c
@@ -0,0 +1,55 @@
+void		to_wedge(const char *str, int n)
+{
+	int		i;
+	int		l;
+
+	if (!str)
+		return ;
+	l = 0;
+	i = n - ft_strlen(str);
+	while (i-- > 0)
+	{
+		ft_putchar(' ');
+		l++;
+	}
+	while (l++ < n)
+		ft_putchar(*str++);
+}
+
+void		to_wedge2(const char *str, int n)
+{
+	int		l;
+
+	if (!str)
+		return ;
+	l = 0;
+	while (l++ < n && *str)
+		ft_putchar(*str++);
+	while (l++ <= n)
+		ft_putchar(' ');
+}
+
+void		to_wedge_lli2(long long int nb, int n)
+{
+	int		l;
+
+	l = ft_llilen(nb);
+	ft_putnbrll(nb);
+	while (l++ < n)
+		ft_putchar(' ');
+}
+
+void		to_wedge_lli(long long int nb, int n)
+{
+	int		i;
+	int		l;
+
+	l = 0;
+	i = n - ft_llilen(nb);
+	while (i-- > 0)
+	{
+		ft_putchar(' ');
+		l++;
+	}
+	ft_putnbrll(nb);
+}
