5e1592d6f0e5acba7a3d959aefa4cb1647274964
apierre42 <aurel.chenuil@gmail.com>
2015-03-25 15:04:31 +0100

    ft_timecmp.c

--- sources/ft_timecmp.c
+++ sources/ft_timecmp.c
@@ -1,36 +1,36 @@
-int		ft_rmtimecmp(void *file1, void *file2)
-{
-	t_file	*f1;
-	t_file	*f2;
-
-	f1 = (t_file)file1;
-	f2 = (t_file)file2;
-	return (f1->stats.st_mtime - f2->stats.st_mtime ?
-	f1->stats.st_mtime - f2->stats.st_mtime :
-	ft_rmtimecmp(file1, file2));
-}
-
-int             ft_ratimecmp(void *file1, void *file2)
+int             ft_mtimecmp(void *file1, void *file2)
 {
         t_file  *f1;
         t_file  *f2;
 
         f1 = (t_file)file1;
         f2 = (t_file)file2;
-        return (f1->stats.st_atime - f2->stats.st_atime ?
-        f1->stats.st_atime - f2->stats.st_atime :
+        return (f2->stats.st_mtime - f1->stats.st_mtime ?
+        f2->stats.st_mtime - f1->stats.st_mtime :
+        ft_rmtimecmp(file1, file2));
+}
+
+int             ft_atimecmp(void *file1, void *file2)
+{
+        t_file  *f1;
+        t_file  *f2;
+
+        f1 = (t_file)file1;
+        f2 = (t_file)file2;
+        return (f2->stats.st_atime - f1->stats.st_atime ?
+        f2->stats.st_atime - f1->stats.st_atime :
         ft_ratimecmp(file1, file2));
 }
 
-int             ft_rctimecmp(void *file1, void *file2)
+int             ft_ctimecmp(void *file1, void *file2)
 {
         t_file  *f1;
         t_file  *f2;
 
         f1 = (t_file)file1;
         f2 = (t_file)file2;
-        return (f1->stats.st_ctime - f2->stats.st_ctime ?
-        f1->stats.st_ctime - f2->stats.st_ctime :
+        return (f2->stats.st_ctime - f1->stats.st_ctime ?
+        f2->stats.st_ctime - f1->stats.st_ctime :
         ft_rctimecmp(file1, file2));
 }
 
